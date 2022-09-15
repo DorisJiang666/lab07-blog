349c5c9dd803b321e21f805afa62499c97fc8afa
DorisJiang666 <20934696@students.latrobe.edu.au>
2022-09-15 13:18:20 +1000

    Third commit

--- api/src/models/post.js
+++ api/src/models/post.js
@@ -3,11 +3,11 @@ module.exports = function(sequelize, DataTypes) {
   var Post = sequelize.define('Post', {
     title: {
       allowNull:false,
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
     validate: { notEmpty: true }},
     content:{
       allowNull:false,
-      type:DataTypes.TEXT
+      type:DataTypes.TEXT,
     validate: { notEmpty: true }}
     }, {
     classMethods: {
